84d943cd56fe77c890c3bfab539efa0450e3ffbc
ShreyaKashi <shreyakashi98@gmail.com>
2017-03-21 15:33:57 +0530

    My First Commit

---
+++ Pro_imp/Assets/Scripts/ManageScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ManageScene : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			SceneManager.LoadScene ("SecondaryRed");
+		}
+	}
+}
